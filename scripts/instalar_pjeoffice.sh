5de9f24381af2438b1ab3a93363586489d3826ae
Pedro Henrique Quitete Barreto <60863081+pedrohqb@users.noreply.github.com>
2025-11-13 23:45:35 -0300

    Update instalar_pjeoffice.sh

--- scripts/instalar_pjeoffice.sh
+++ scripts/instalar_pjeoffice.sh
@@ -3,6 +3,7 @@
 
 # Variáveis
 URL_PJEOFFICE="https://github.com/pedrohqb/pje-office-debian/releases/download/2.5.16u-2/pje-office_2.5.16u-2_amd64.deb"
+ARQUIVO_DEB="${HOME}/Downloads/pje-office_2.5.16u-2_amd64.deb"
 # SHA256 do arquivo DEB
 CHECKSUM_PJEOFFICE="02ee084b311027900f2b79ee8c73e3aa8fb91dfb509e3f7e75c144c37166fe07"
 

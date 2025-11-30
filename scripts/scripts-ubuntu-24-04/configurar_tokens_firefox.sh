626fe9bbd2166c8f6812898f46e0d733c6d21a09
Pedro Henrique Quitete Barreto <60863081+pedrohqb@users.noreply.github.com>
2025-11-30 01:38:34 -0300

    Update configurar_tokens_firefox.sh

--- scripts/scripts-ubuntu-24-04/configurar_tokens_firefox.sh
+++ scripts/scripts-ubuntu-24-04/configurar_tokens_firefox.sh
@@ -17,7 +17,7 @@ firefox --headless --new-tab about:blank &
 sleep 6
 
 # Mata todos os processos do Firefox para garantir que o navegador esteja fechado
-pkill -f firefox > /dev/null 2>&1
+pkill -f firefox
 echo "Processo do Firefox encerrado."
 echo "---"
 

08995cc7f242623d7a07ab196ca85f0bd966b94d
Pedro Henrique Quitete Barreto <60863081+pedrohqb@users.noreply.github.com>
2026-02-01 00:50:19 -0300

    Update instalar_pjeoffice.sh

--- scripts/instalar_pjeoffice.sh
+++ scripts/instalar_pjeoffice.sh
@@ -2,10 +2,10 @@
 # Script de Instalação do PJeOffice
 
 # Variáveis
-URL_PJEOFFICE="https://github.com/pedrohqb/pje-office-debian/releases/download/2.5.16u-2/pje-office_2.5.16u-2_amd64.deb"
-ARQUIVO_DEB="${HOME}/Downloads/pje-office_2.5.16u-2_amd64.deb"
+URL_PJEOFFICE="https://github.com/pedrohqb/pje-office-debian/releases/download/2.5.16u-3/pje-office_2.5.16u-3_amd64.deb"
+ARQUIVO_DEB="${HOME}/Downloads/pje-office_2.5.16u-3_amd64.deb"
 # SHA256 do arquivo DEB
-CHECKSUM_PJEOFFICE="02ee084b311027900f2b79ee8c73e3aa8fb91dfb509e3f7e75c144c37166fe07"
+CHECKSUM_PJEOFFICE="d91510730355ebb82cfc7a810a37840392fb25947422ce29ddf6f63a56775cc7"
 
 echo "Baixando PJeOffice..."
 wget -P "${HOME}/Downloads" "${URL_PJEOFFICE}"

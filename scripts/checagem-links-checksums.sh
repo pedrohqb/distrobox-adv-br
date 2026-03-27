8a0c5f0decb64d2b16cfae7e174d35bfe492ba65
Pedro Henrique Quitete Barreto <60863081+pedrohqb@users.noreply.github.com>
2026-03-27 00:59:57 -0300

    Update checagem-links-checksums.sh

--- scripts/checagem-links-checksums.sh
+++ scripts/checagem-links-checksums.sh
@@ -16,7 +16,7 @@ check_list=(
     "PJeOffice|https://github.com/pedrohqb/pje-office-debian/releases/download/2.5.16u-3/pje-office_2.5.16u-3_amd64.deb|d91510730355ebb82cfc7a810a37840392fb25947422ce29ddf6f63a56775cc7"
     "SafeNet SAC|https://www.digicert.com/StaticFiles/Linux_SAC_10.9_GA.zip|46759cfe91d736af18a49d10e4749f264022db44e04ed4caf94e1ca77d6a013e"
     "SafeSign IC|https://assets.ctfassets.net/zuadwp3l2xby/2loCJpZkGMJe5y06gwzM4C/a065ab00cd0c42ca04de8870dce3de06/SafeSign_IC_Standard_Linux_ub2404_4.2.1.0-AET.000.zip|3cf3e94ca8dddefe4e192c2a84a14cbdfd0789271d02d7d323da0b48ecdb8ac7"
-    "SerproID|https://serprodrive.serpro.gov.br/s/Cc3EbToE9AFq4qX/download|0ffa9ffe5bc343cc758a12f28bd7f08aec4b6e843d1c043baf0b81572461e588"
+    "SerproID|https://storagegw.estaleiro.serpro.gov.br/instalador-desktop/SerproID-2.1.6-amd64.deb|0ffa9ffe5bc343cc758a12f28bd7f08aec4b6e843d1c043baf0b81572461e588"
     "Lacuna Web PKI|https://get.webpkiplugin.com/Downloads/2.14.0/setup-deb-64|24e132519f9f6b8a51553e5f4e6cc33d42a63f92c8d7db870563be1b759a00fe"
     "Softplan WebSigner|https://websigner.softplan.com.br/Downloads/2.12.1/webpki-chrome-64-deb|5da8fd36f1371f52bbaebede75fade1928f09cff2dd605b8da5663c6da505379"
 )
